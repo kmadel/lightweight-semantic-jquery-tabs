5c91c581868e10b7f221220546ea4591478da151
Kurt Madel <kmadel@mac.com>
2012-01-03 12:29:30 -0500

    Added support for dynamic width based
    Basically used jQuery to determine necessary width of dd elements

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -13,9 +13,14 @@
 			
 			if (el.find('dt a[href="'+hash+'"]').length) {
 				current = el.find('a[href="'+hash+'"]').parent().addClass('current');
+				var dd = el.find('a[href="'+hash+'"]').parent().next('dd');
 			} else {
-				current = el.find('dt:first').addClass('current');
+				var dd = el.find('dd:first');
+        		current = el.find('dt:first').addClass('current');
 			}
+		    //get padding with borders
+		    var wPadding = dd.outerWidth() - dd.width();
+		    dd.width(el.width()-wPadding);
 			
 			var currentHeight = current.next('dd').show().height();
 			el.css('height', dlHeight + currentHeight);
@@ -24,13 +29,21 @@
 
 		$('dl.enhance dt a').click(function(e){
 			e.preventDefault();
-
-			$(this).parents('dl').find('.current').removeClass('current').next('dd').hide();
-			var current = $(this).parent('dt').addClass('current');
-			var currentHeight = current.next('dd').show().height();
-			var dlHeight = $(this).parents('dl').removeAttr('style').height();
 			
-			$(this).parents('dl').css('height', dlHeight + currentHeight);
+			var dl = $(this).parents('dl');
+
+			dl.find('.current').removeClass('current').next('dd').hide();
+			var current = $(this).parent('dt').addClass('current');
+
+	    	var dd = current.next('dd');
+	    	//get padding with borders
+	    	var wPadding = dd.outerWidth() - dd.width();
+	    	dd.width(dl.width()-wPadding);
+			
+			var currentHeight = current.next('dd').show().height();
+			var dlHeight = dl.removeAttr('style').height();
+			
+			dl.css('height', dlHeight + currentHeight);
 			
 			location.hash = $(this).attr('href');
 		});
